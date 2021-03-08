69e94878d73b57960fd99adb9688840ae475a3f6
Alan Lima <alan.lima@hartbgroup.com.br>
2021-03-08 16:20:27 -0300

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Alan Lima on 08/03/21.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
